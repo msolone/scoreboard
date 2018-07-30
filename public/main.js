8120e99504d4ef32a9902a6aa7394b4b8d633b38
Michael Solone <michaelsolone@gmail.com>
2018-07-30 16:33:55 -0400

    finished basic score bored

--- public/main.js
+++ public/main.js
@@ -35,54 +35,122 @@ document.querySelector('.update-awayteam-name').addEventListener('click', update
 
 
 
-const addHometeamScore = () => {
+const TouchdownHometeamScore = () => {
+  // Finding the Home Teams current score
+  const currentScore = parseInt(document.querySelector('.home-score-numbers').textContent)
+  // Setting newScore to currentScore + 1
+  const newScore = currentScore + 6
+  // Changing the home-score-numbers to what the new score is
+  document.querySelector('.home-score-numbers').textContent = newScore
+
+}
+// when 'touchdown' button is clicked, the 'TouchdownHometeamScore' function will run
+document.querySelector('.touchdown').addEventListener('click', TouchdownHometeamScore) 
+
+
+
+const extraPointHometeam = () => {
   // Finding the Home Teams current score
   const currentScore = parseInt(document.querySelector('.home-score-numbers').textContent)
   // Setting newScore to currentScore + 1
   const newScore = currentScore + 1
   // Changing the home-score-numbers to what the new score is
   document.querySelector('.home-score-numbers').textContent = newScore
-
 }
-// when 'add-hometeam-score' button is clicked, the 'addHometeamScore' function will run
-document.querySelector('.add-hometeam-score').addEventListener('click', addHometeamScore) 
+// when 'extra-point' button is clicked, the 'extrapointHometeamScore' function will run
+document.querySelector('.extra-point').addEventListener('click', extraPointHometeam)
 
 
 
-const subtractHometeamScore = () => {
+const goForTwoHometeam = () => {
   // Finding the Home Teams current score
   const currentScore = parseInt(document.querySelector('.home-score-numbers').textContent)
-  // Setting newScore to currentScore - 1
-  const newScore = currentScore - 1
+  // Setting newScore to currentScore + 2
+  const newScore = currentScore + 2
   // Changing the home-score-numbers to what the new score is
   document.querySelector('.home-score-numbers').textContent = newScore
+}
+// when 'field-goal' button is clicked, the 'fieldgoalHometeamScore' function will run
+document.querySelector('.go-for-two').addEventListener('click', goForTwoHometeam)
+
+
+
+
+const fieldgoalHometeam = () => {
+  // Finding the Home Teams current score
+  const currentScore = parseInt(document.querySelector('.home-score-numbers').textContent)
+  // Setting newScore to currentScore + 3
+  const newScore = currentScore + 3
+  // Changing the home-score-numbers to what the new score is
+  document.querySelector('.home-score-numbers').textContent = newScore
+}
+// when 'field-goal' button is clicked, the 'fieldgoalHometeamScore' function will run
+document.querySelector('.field-goal').addEventListener('click', fieldgoalHometeam)
+
+
+
+
+
+
+const TouchdownAwayteamScore = () => {
+  // Finding the Away Teams current score
+  const currentScore = parseInt(document.querySelector('.away-score-numbers').textContent)
+  // Setting newScore to currentScore + 1
+  const newScore = currentScore + 6
+  // Changing the away-score-numbers to what the new score is
+  document.querySelector('.away-score-numbers').textContent = newScore
 
 }
-// when 'subtract-hometeam-score' button is clicked, the 'subtractHometeamScore' function will run
-document.querySelector('.subtract-hometeam-score').addEventListener('click', subtractHometeamScore) 
+// when 'touchdown2' button is clicked, the 'TouchdownAwayteamScore' function will run
+document.querySelector('.touchdown2').addEventListener('click', TouchdownAwayteamScore) 
 
 
-const addAwayteamScore = () => {
+
+const extraPointAwayteam = () => {
   // Finding the Away Teams current score
   const currentScore = parseInt(document.querySelector('.away-score-numbers').textContent)
   // Setting newScore to currentScore + 1
   const newScore = currentScore + 1
   // Changing the away-score-numbers to what the new score is
   document.querySelector('.away-score-numbers').textContent = newScore
-
 }
-// when 'add-awayteam-score' button is clicked, the 'addAwayteamScore' function will run
-document.querySelector('.add-awayteam-score').addEventListener('click', addAwayteamScore) 
+// when 'extra-point2' button is clicked, the 'extrapointHometeamScore' function will run
+document.querySelector('.extra-point2').addEventListener('click', extraPointAwayteam)
 
 
-const subtractAwayteamScore = () => {
+
+const goForTwoAwayteam = () => {
   // Finding the Away Teams current score
   const currentScore = parseInt(document.querySelector('.away-score-numbers').textContent)
-  // Setting newScore to currentScore - 1
-  const newScore = currentScore - 1
+  // Setting newScore to currentScore + 2
+  const newScore = currentScore + 2
   // Changing the away-score-numbers to what the new score is
   document.querySelector('.away-score-numbers').textContent = newScore
-
 }
-// when 'subtract-awayteam-score' button is clicked, the 'subtractAwayteamScore' function will run
-document.querySelector('.subtract-awayteam-score').addEventListener('click', subtractAwayteamScore)
+// when 'go-for-two2' button is clicked, the 'fieldgoalAwayteamScore' function will run
+document.querySelector('.go-for-two2').addEventListener('click', goForTwoAwayteam)
+
+
+
+
+const fieldgoalAwayteam = () => {
+  // Finding the Away Teams current score
+  const currentScore = parseInt(document.querySelector('.away-score-numbers').textContent)
+  // Setting newScore to currentScore + 3
+  const newScore = currentScore + 3
+  // Changing the away-score-numbers to what the new score is
+  document.querySelector('.away-score-numbers').textContent = newScore
+}
+// when 'field-goal2' button is clicked, the 'fieldgoalAwayteamScore' function will run
+document.querySelector('.field-goal2').addEventListener('click', fieldgoalAwayteam)
+
+
+
+
+
+
+
+
+ 
+
+
